08f42dfee1c3af49a794dc2119f1c1a1b7445cec
Alejandro Hortal Valor <alejandrohortalvalor02@gmail.com>
2025-03-08 00:49:58 +0100

    Implementación en ambas screens del componente reutilizable

--- src/client/screens/LoginScreen.tsx
+++ src/client/screens/LoginScreen.tsx
@@ -1,51 +1,35 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
 import { loginStyles as styles } from '../styles/loginStyles';
 import { useLogin } from '../hooks/useLogin';
+import CustomTextInput from '../components/CustomInputText';
+
 
 const LoginScreen = ({ navigation }: { navigation: NavigationProp<any> }) => {
-  const {
-    username, setUsername,
-    password, setPassword,
-    handleLogin
-  } = useLogin(navigation);
+  const { username, setUsername, password, setPassword, handleLogin } = useLogin(navigation);
+
   return (
-    <ImageBackground
-      source={require('../../assets/images/fondoAuth.jpg')}
-      style={styles.background}
-    >
+    <ImageBackground source={require('../../assets/images/fondoAuth.jpg')} style={styles.background}>
       <View style={styles.container}>
         <Text style={styles.title}>Bienvenid@</Text>
         <Text style={styles.subtitle}>Inicia sesión para continuar</Text>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Usuario"
-          placeholderTextColor="#999"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Contraseña"
-          placeholderTextColor="#999"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
+        <CustomTextInput placeholder="Usuario" value={username} onChangeText={setUsername} />
+        <CustomTextInput placeholder="Contraseña" value={password} onChangeText={setPassword} secureTextEntry />
 
         <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
           <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-          <Text style={styles.registerText}>¿No tienes una cuenta? <Text style={styles.registerLink}>Regístrate</Text></Text>
+          <Text style={styles.registerText}>
+            ¿No tienes una cuenta? <Text style={styles.registerLink}>Regístrate</Text>
+          </Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
   );
 };
-
 
 export default LoginScreen;
--- src/client/screens/RegisterScreen.tsx
+++ src/client/screens/RegisterScreen.tsx
@@ -1,56 +1,31 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, ImageBackground, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
 import { registerStyles as styles } from '../styles/registerStyles';
 import { useRegister } from '../hooks/useRegister';
+import CustomTextInput from '../components/CustomInputText';
+
 const RegisterScreen = ({ navigation }: { navigation: NavigationProp<any> }) => {
-  const {
-    username, setUsername,
-    password, setPassword,
-    confirmPassword, setConfirmPassword,
-    handleRegister,
-  } = useRegister(navigation);
+  const { username, setUsername, password, setPassword, confirmPassword, setConfirmPassword, handleRegister } = useRegister(navigation);
 
   return (
-    <ImageBackground
-      source={require('../../assets/images/fondoAuth.jpg')}
-      style={styles.background}
-    >
+    <ImageBackground source={require('../../assets/images/fondoAuth.jpg')} style={styles.background}>
       <View style={styles.container}>
         <Text style={styles.title}>Registrarse</Text>
         <Text style={styles.subtitle}>Crea una cuenta para continuar</Text>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Nombre de usuario"
-          placeholderTextColor="#999"
-          value={username}
-          onChangeText={setUsername}
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Contraseña"
-          placeholderTextColor="#999"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Confirmar Contraseña"
-          placeholderTextColor="#999"
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-          secureTextEntry
-        />
+        <CustomTextInput placeholder="Nombre de usuario" value={username} onChangeText={setUsername} autoCapitalize="none" />
+        <CustomTextInput placeholder="Contraseña" value={password} onChangeText={setPassword} secureTextEntry />
+        <CustomTextInput placeholder="Confirmar Contraseña" value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry />
 
         <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
           <Text style={styles.registerButtonText}>Registrarse</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.loginText}>¿Ya tienes una cuenta? <Text style={styles.loginLink}>Inicia Sesión</Text></Text>
+          <Text style={styles.loginText}>
+            ¿Ya tienes una cuenta? <Text style={styles.loginLink}>Inicia Sesión</Text>
+          </Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
